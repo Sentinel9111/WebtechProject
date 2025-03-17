98f37fc92798a93c03451ea62fb78dc0bd04b9c5
Sentinel9111 <92266462+Sentinel9111@users.noreply.github.com>
2025-03-17 15:16:00 +0100

    update app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, IntegerField, SelectField
+from wtforms.validators import DataRequired, NumberRange
 
 app = Flask(__name__)
+app.secret_key = "Banaan"
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
