f90f9cd3ff98e02ed692999eb7b7b997e102f27a
Sentinel9111 <92266462+Sentinel9111@users.noreply.github.com>
2025-03-17 15:42:48 +0100

    changes

--- app.py
+++ app.py
@@ -6,11 +6,20 @@ from wtforms.validators import DataRequired, NumberRange
 app = Flask(__name__)
 app.secret_key = "Banaan"
 
-
+db.init_app(app)
+@app.before_request
+def create_tables():
+    db.create_all()
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# @app.route('/loginForm', methods=['GET', 'POST'])
+# def loginForm():
+#     form = loginForm()
+#     if form.validate_on_submit():
+#         naam = form.naam.data
+#         return redirect(url_for(''))
 
 if __name__ == '__main__':
     app.run()
