6f69f8e254c15f40e3338363ea3260e9ca94816b
Sentinel9111 <92266462+Sentinel9111@users.noreply.github.com>
2025-03-18 11:06:59 +0100

    added models.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, IntegerField, SelectField
 from wtforms.validators import DataRequired, NumberRange
+from models import db
 
 app = Flask(__name__)
 app.secret_key = "Banaan"
