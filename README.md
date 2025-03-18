291b3649921df3338aa761fe6df7e4028cfc058a
Sentinel9111 <92266462+Sentinel9111@users.noreply.github.com>
2025-03-18 17:30:49 +0100

    unfinished update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# WebtechProject
+# Project Webtechnologie
+# Inventarissysteem
+
+
+
+
--- app.py
+++ app.py
@@ -1,11 +1,20 @@
+
 from flask import Flask, Blueprint, render_template, request, redirect, url_for, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, IntegerField, SelectField
 from wtforms.validators import DataRequired, NumberRange
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 from models import db, User
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = "Banaan"
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db.init_app(app)
 @app.before_request
@@ -16,7 +25,7 @@ def create_tables():
 def index():
     return render_template('index.html')
 
-# @app.route('/loginForm', methods=['GET', 'POST'])
+# @app.route('/login', methods=['GET', 'POST'])
 # def loginForm():
 #     form = loginForm()
 #     if form.validate_on_submit():
