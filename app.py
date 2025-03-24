6a73d2664eabfbec5d9c2649b30ee864f044dc67
Sentinel9111 <92266462+Sentinel9111@users.noreply.github.com>
2025-03-24 16:01:50 +0100

    waarom werkt dit niet 😭😭

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, Blueprint, render_template, request, redirect, url_for, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, IntegerField, SelectField
@@ -6,7 +5,6 @@ from wtforms.validators import DataRequired, NumberRange
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from models import db, User
 from dotenv import load_dotenv
 import os
 
@@ -16,21 +14,20 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
 db.init_app(app)
 @app.before_request
 def create_tables():
     db.create_all()
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-# @app.route('/login', methods=['GET', 'POST'])
-# def loginForm():
-#     form = loginForm()
-#     if form.validate_on_submit():
-#         naam = form.naam.data
-#         return redirect(url_for(''))
+from routes import *
+from models import db
 
 if __name__ == '__main__':
     app.run(debug=True)
