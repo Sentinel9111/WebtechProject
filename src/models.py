755e5c17eaf95b2e26278e25ee512d88a8f2d329
Bauke Westendorp <baukewestendorp@gmail.com>
2025-04-15 11:17:12 +0200

    Maak DB model representatie af

--- src/models.py
+++ src/models.py
@@ -11,5 +11,69 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
 
-def __repr__(self):
-    return f"User('{self.username}', '{self.email}')"
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
+class Job(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200), nullable=True)
+
+    def __repr__(self):
+        return f"Job('{self.id}', '{self.name}', '{self.description}')"
+
+class UserJob(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    job_id = db.Column(db.Integer, db.ForeignKey('job.id'), nullable=False)
+
+    def __repr__(self):
+        return f"UserJob('{self.user_id}', '{self.job_id}')"
+
+class Equipment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    brand = db.Column(db.String(100), nullable=False)
+    category = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Equipment('{self.id}', '{self.name}', '{self.brand}', '{self.category}')"
+
+class Cable(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    length = db.Column(db.Float, nullable=False)
+    conn_a_id = db.Column(db.Integer, db.ForeignKey('connector.id'), nullable=False)
+    conn_b_id = db.Column(db.Integer, db.ForeignKey('connector.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Cable('{self.id}', '{self.name}', '{self.category_id}', '{self.length}', '{self.conn_a_id}', '{self.conn_b_id}')"
+
+class EquipmentJob(db.Model):
+    equipment_id = db.Column(db.Integer, db.ForeignKey('equipment.id'), nullable=False)
+    job_id = db.Column(db.Integer, db.ForeignKey('job.id'), nullable=False)
+
+    def __repr__(self):
+        return f"EquipmentJob('{self.equipment_id}', '{self.job_id}')"
+
+class CableJob(db.Model):
+    cable_id = db.Column(db.Integer, db.ForeignKey('cable.id'), nullable=False)
+    job_id = db.Column(db.Integer, db.ForeignKey('job.id'), nullable=False)
+
+    def __repr__(self):
+        return f"CableJob('{self.cable_id}', '{self.job_id}')"
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f"Category('{self.id}', '{self.name}')"
+
+class Connector(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    is_male = db.Column(db.Boolean, nullable=False)
+
+    def __repr__(self):
+        return f"Connector('{self.id}', '{self.name}', '{self.is_male}')"
