65ee645d4531289782f55bc316ce68fd3427a32b
Bauke Westendorp <baukewestendorp@gmail.com>
2025-04-15 15:22:12 +0200

    WIP

--- src/models.py
+++ src/models.py
@@ -12,7 +12,7 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(60), nullable=False)
 
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}')"
+        return f"User('{self.id}', {self.username}', '{self.email}')"
 
 class Job(db.Model):
     id = db.Column(db.Integer, primary_key=True)
